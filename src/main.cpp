bc58d451b0a00587556830b86d462c29e21ed33d
Forward98 <m15256916478@163.com>
2023-12-08 21:10:54 +0800

    win10_VScode多文件编译调试框架

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,10 @@ using namespace std;
 int main() {
     int a = 10;
     int b = 20;
+    cout << "before swap:" <<endl;
     cout << a << " " << b << endl;
     swap(a, b);
+    cout << "after swap:" <<endl;
     cout << a << " " << b << endl;
     
     return 0;
